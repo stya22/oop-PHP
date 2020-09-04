1e5a5c2d2b701cc5f4bc3f4e74f26a715e97155b
stya22 <styatube@gmail.com>
2020-09-04 07:20:04 +0700

    Release 1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once "animal.php";
+include_once "ape.php";
+include_once "frog.php";
 $sheep = new Animal("shaun");
 
 echo $sheep->name; // "shaun"
@@ -8,3 +10,12 @@ echo "<br>";
 echo $sheep->legs; // 2
 echo "<br>";
 echo $sheep->cold_blooded; // false
+
+// index.php
+echo "<br>";
+$sungokong = new Ape("kera sakti");
+$sungokong->yell(); // "Auooo"
+
+echo "<br>";
+$kodok = new Frog("buduk");
+$kodok->jump(); // "hop hop"
